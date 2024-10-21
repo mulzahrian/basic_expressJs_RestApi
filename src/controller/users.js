ceee439bec6b3e0d2851b884afe1ee6f7d76087a
mulzahrian <mulzahrianone@gmail.com>
2024-10-21 16:42:01 +0700

    Add another CRUD

--- src/controller/users.js
+++ src/controller/users.js
@@ -16,31 +16,58 @@ const getAllUser = async (req, res) => {
   }
 };
 
-const createNewUser = (req, res) => {
-  console.log(req.body);
-  res.json({
-    message: "Create User Success",
-    data: req.body,
-    status: 200,
-  });
+const createNewUser = async (req, res) => {
+  const bodyPayload = req.body;
+  try {
+    await usersModels.createNewUser(bodyPayload);
+    res.json({
+      message: "Create User Success!",
+      data: bodyPayload,
+      status: 200,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error!",
+      serverMessage: error,
+    });
+  }
 };
 
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
   const { id } = req.params;
-  console.log("id", id);
-  res.json({
-    message: "Update User Success",
-    status: 200,
-    data: req.body,
-  });
+  const { body } = req;
+  try {
+    await usersModels.updateUser(body, id);
+    res.json({
+      message: "Update User Success",
+      status: 200,
+      data: {
+        id: id,
+        ...body,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error!",
+      serverMessage: error,
+    });
+  }
 };
 
-const deleteUser = (req, res) => {
+const deleteUser = async (req, res) => {
   const { id } = req.params;
-  res.json({
-    message: "Delete User Success",
-    status: 200,
-  });
+  try {
+    await usersModels.deleteUser(id);
+    res.json({
+      message: "Delete User Success",
+      status: 200,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server Error!",
+      serverMessage: error,
+    });
+  }
 };
 
 module.exports = {
--- src/models/users.js
+++ src/models/users.js
@@ -5,6 +5,26 @@ const getAllUser = () => {
   return Dbpool.execute(SQLQuery);
 };
 
+const createNewUser = (body) => {
+  const SQLQuery = `INSERT INTO users (nama,email,tempat) 
+                    VALUE ('${body.nama}','${body.email}','${body.tempat}')`;
+  return Dbpool.execute(SQLQuery);
+};
+
+const updateUser = (body, id) => {
+  const SQLQuery = `UPDATE users SET nama='${body.nama}',email='${body.email}',tempat='${body.tempat}'
+                    WHERE id=${id}`;
+  return Dbpool.execute(SQLQuery);
+};
+
+const deleteUser = (id) => {
+  const SQLQuery = `DELETE FROM users WHERE id=${id}`;
+  return Dbpool.execute(SQLQuery);
+};
+
 module.exports = {
   getAllUser,
+  createNewUser,
+  updateUser,
+  deleteUser,
 };
